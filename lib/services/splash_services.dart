c632076f8919bb45545a97a4df79424147ded92f
PrasiddhaKhadka <prasiddha@thefireflytech.com>
2024-06-14 11:17:52 +0545

    Add: User model

---
+++ lib/services/splash_services.dart
@@ -0,0 +1,31 @@
+import 'dart:async'; // Importing dart:async for asynchronous operations
+
+import 'package:flutter/material.dart'; // Importing Flutter material library
+
+/// A class containing services related to the splash screen.
+class SplashServices {
+  /// Checks authentication status and navigates accordingly.
+  ///
+  /// Takes a [BuildContext] as input and navigates to the home screen if the user is authenticated,
+  /// otherwise navigates to the login screen after a delay of 2 seconds.
+  void checkAuthentication(BuildContext context) async {
+    // SessionController().getUserFromPreference().then((value) async {
+    //   if (SessionController.isLogin ?? false) {
+    //     Timer(
+    //       const Duration(seconds: 2),
+    //       () => Navigator.pushNamedAndRemoveUntil(context, RoutesName.home, (route) => false),
+    //     );
+    //   } else {
+    //     Timer(
+    //       const Duration(seconds: 2),
+    //       () => Navigator.pushNamedAndRemoveUntil(context, RoutesName.login, (route) => false),
+    //     );
+    //   }
+    // }).onError((error, stackTrace) {
+    //   Timer(
+    //     const Duration(seconds: 2),
+    //     () => Navigator.pushNamedAndRemoveUntil(context, RoutesName.login, (route) => false),
+    //   );
+    // });
+  }
+}
---
+++ lib/view/splash/splash_view.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+/// A widget representing the splash screen of the application.
+class SplashView extends StatefulWidget {
+  const SplashView({Key? key}) : super(key: key);
+
+  @override
+  State<SplashView> createState() => _SplashViewState();
+}
+
+/// The state of the [SplashView] widget.
+class _SplashViewState extends State<SplashView> {
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   // Calls the [checkAuthentication] method from [SplashServices] to handle authentication logic
+  //   splashServices.checkAuthentication(context);
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          'Splash Screen',
+          // Displays the localized text for the splash screen
+          // AppLocalizations.of(context)!.splashScreen,
+          // style: Theme.of(context)
+          //     .textTheme
+          //     .displayMedium, // Applies the displayMedium text style from the current theme
+        ),
+      ),
+    );
+  }
+}
